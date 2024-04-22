4e88a3fe9a2b5cdba847896c1c034fc6101ab6e0
Julio Daniel Azurduy <113427338+DanJuaz@users.noreply.github.com>
2024-04-22 11:42:48 +0200

    Delete versionJavaFx-19 directory

--- versionJavaFx-19/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.donostiluxdrive {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.donostiluxdrive to javafx.fxml;
-    exports com.example.donostiluxdrive;
-}
